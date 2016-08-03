664ec58d719faa375f3f5a54ee62b8865bcde8d1
BlueSkeye <bsgithub@gmail.com>
2016-08-03 20:51:07 +0200

    Sedding started testing consensus download

---
+++ TorNetTester/ConsensusTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TorNet;
+using TorNet.Tor;
+
+namespace TorNetTester
+{
+    [TestClass]
+    public class ConsensusTests
+    {
+        public ConsensusTests()
+        {
+        }
+
+        public TestContext TestContext { get; set; }
+
+        #region Attributs de tests supplémentaires
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        /// <summary>Fetch consensus ignoring cache.</summary>
+        [TestMethod]
+        public void DownloadConsensus()
+        {
+            Consensus consensus = new Consensus(Consensus.Options.force_download);
+            return;
+        }
+    }
+}
